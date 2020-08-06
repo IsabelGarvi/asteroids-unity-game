8962c3e5f80404bc9877a39881a8c09198fa642a
Izz Garvi <igarvilo@gmail.com>
2020-08-06 15:48:02 +0100

    rotation of ship to look at mouse on screen

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	void movePlayer(){
+
+	}
+
+	void shootProjectile(){
+
+	}
+
+	void lookAtMouse(){
+		Vector3 targetDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		float rotate = (Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg) - 90f;
+		transform.rotation = Quaternion.Euler(0f, 0f, rotate);
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookAtMouse();
+    }
+}
