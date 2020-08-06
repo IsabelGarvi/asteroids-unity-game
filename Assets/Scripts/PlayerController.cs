995cb78da1ac852d36da1becd10cfe8a5e419276
Izz Garvi <igarvilo@gmail.com>
2020-08-06 16:09:04 +0100

    move the player to where the mouse is on the scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	Vector3 targetDirection = Vector3.zero;
 
-	void movePlayer(){
+	private void movePlayer(){
+		/* Move the ship towards where the mouse is.
+		*/
+
+		targetDirection = get_target_direction();
+		if(Input.GetKey(KeyCode.W)){
+			transform.position = Vector3.MoveTowards(transform.position, targetDirection, 2 * Time.deltaTime);
+		}
 
 	}
 
-	void shootProjectile(){
+	private void shootProjectile(){
 
 	}
 
-	void lookAtMouse(){
-		Vector3 targetDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+	private void lookAtMouse(){
+		/* Make the ship look at where the mouse is on the scene
+		*/
+		targetDirection = get_target_direction();
 		float rotate = (Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg) - 90f;
 		transform.rotation = Quaternion.Euler(0f, 0f, rotate);
+	}
+
+	private Vector3 get_target_direction(){
+		/* Get the direction towards where the ship will be pointing
+		*/
+		return Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 	}
 
     // Start is called before the first frame update
@@ -28,6 +44,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    	movePlayer();
         lookAtMouse();
     }
 }
