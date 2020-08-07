92c0e043ddeede88914e049574bac423103e51da
Izz Garvi <igarvilo@gmail.com>
2020-08-07 13:43:47 +0100

    create ship when scene started

--- Assets/Scripts/Level01.cs
+++ Assets/Scripts/Level01.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Level01 : MonoBehaviour
 {
-	public GameObject big_asteroid;
+	public GameObject big_asteroid, ship;
 
     // Start is called before the first frame update
     void Start()
     {
         // create player at the center of the screen
-        // create random number of big asteroids (between 5 and 10)
+        createShip();
         int big_asteroids_number = Random.Range(5, 10);
         createBigAsteroids(big_asteroids_number);
         // initialise GUI
@@ -22,6 +22,10 @@ public class Level01 : MonoBehaviour
         	Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
             GameObject new_big_asteroid = Instantiate(big_asteroid, position, transform.rotation);
         }
+    }
+
+    private void createShip(){
+    	GameObject new_ship = Instantiate(ship, Vector3.zero, transform.rotation);
     }
 
     // Update is called once per frame
